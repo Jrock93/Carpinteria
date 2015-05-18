30233f90c6342711e9898444ac38e55377e881a8
J'rock <aurockjose@gmail.com>
2015-05-18 12:57:05 -0600

    Modificado Con Slide de imagenes 1

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(Slider);
+
+
+
+function Slider() {
+	// body...
+	
+	$("#btAnt").click(function(){
+
+
+
+	});
+	$("#btSig").click(function()
+	{
+		var cont = $('.imagenes').find('.i_elemento').size();
+		$('.imagenes').find('.i_elemento').each(
+			
+			function(index, value){
+				if($(value).hasClass('i_visible')){
+					$(value).slideUp();
+					$(value).removeClass('i_visible');
+					
+					
+					if(index+1<cont){
+						$($('.imagenes').find('.i_elemento').get(index+1)).slideDown();
+						$($('.imagenes').find('.i_elemento').get(index+1)).addClass('i_visible');	
+						return false;
+					}	
+				
+				else{
+					$($('.imagenes').find('.i_elemento').get(0)).slideDown();
+					$($('.imagenes').find('.i_elemento').get(0)).addClass('i_visible');
+					return false;
+				}
+				}
+
+
+		});
+		
+		
+		
+	});
+
+
+
+}
